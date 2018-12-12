c61efd9e7e78b79f5a0279c6391ce895484e2e8d
jjtfsalgado <jjtfsalgado@gmail.com>
2018-12-12 13:24:54 +0000

    Did some progress on the top users purchases kata

--- top_users_purchases.js
+++ top_users_purchases.js
@@ -6,7 +6,7 @@ var a3 = ['A042', 'A025', 'B004'];
 
 function idBestUsers(a1, a2, a3) {
     // your code here
-
+    const result = [];
     const a1Data = a1.map(i => ({type: 1, item: i}));
     const a1Data1 = a2.map(i => ({type: 2, item: i}));
     const a1Data2 = a3.map(i => ({type: 3, item: i}));
@@ -33,6 +33,18 @@ function idBestUsers(a1, a2, a3) {
         return accum;
     }, {});
 
+    for(let i = Object.keys(i); i > 0; i++){
+        const item = d[i];
+        let counter = 0;
+        const itemKeys = Object.keys(item).length;
+
+        if(itemKeys === 3){
+            for(let j = itemKeys; j > 0; j++){
+                counter += item[j]
+            }
+            result.push()
+        }
+    }
 
     return d;
 }
