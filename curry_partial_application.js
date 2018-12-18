47a2161bf595459fc19dcb78a4c9cef54a28b18e
jjtfsalgado <jjtfsalgado@gmail.com>
2018-12-18 12:56:50 +0000

    Did some progress on the currying/partial kata

--- curry_partial_application.js
+++ curry_partial_application.js
@@ -1,2 +1,19 @@
 function curryPartial(fn) {
+    const args = Array.prototype.slice.call(arguments, 1);
+    const result = 0;
+    // console.log(fn);
+
+    // const f = (ar) => {
+    //     ar ?  f(ar) : "result";
+    // };
+
+    return (a) => a ? curryPartial(a) : result;
 }
+
+function add(x, y, z) {
+    return x + y + z;
+}
+
+console.log(curryPartial(add)(1)(2)(3)); //6
+// curryPartial(add, 0, 1)(2)
+// (2)(3); //6
