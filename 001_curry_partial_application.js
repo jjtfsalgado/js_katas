bb233715dd51e167424194497733ecb9fa9348ae
jjtfsalgado <jjtfsalgado@gmail.com>
2019-01-03 20:20:19 +0000

    Finished currying kata

--- 001_curry_partial_application.js
+++ 001_curry_partial_application.js
@@ -2,21 +2,24 @@ function curryPartial(fn){
     const parameters = Array.prototype.slice.call(arguments, 1);
     function f(len, args) {
         return (...a) => {
-            if(len - args.length < 2){
-                return fn(...a, ...args);
+            if((len - (args.length + a.length) < 1) && a.length){
+                return fn(...args.concat(a));
             }
-            return f(len - args.length, [...a, ...args]);
+            return f(len, args.concat(a));
         }
     }
 
-    return fn.length === parameters.length ? fn(...parameters) : f(fn.length, parameters);
+    return fn.length <= parameters.length ? fn(...parameters) : f(fn.length, parameters);
 }
 
-function add(x, y, z) {
-    return x + y + z;
+function add(a, b, c) {
+    return a + b + c;
 }
 
-console.log(curryPartial(add, 2)(3)(4)); //9
-console.log(curryPartial(add)(2)(3)(4)); //9
-console.log(curryPartial(add, 2, 3)(4)); //9
-console.log(curryPartial(add, 2, 3, 4)); //9
+// console.log(curryPartial(add, 2)(3)(4)); //9
+// console.log(curryPartial(add)(2)(3)(4)); //9
+// console.log(curryPartial(add, 2, 3)(4)); //9
+// console.log(curryPartial(add, 2, 3, 4)); //9
+
+
+//Code wars url -> https://www.codewars.com/kata/53cf7e37e9876c35a60002c9
