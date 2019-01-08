0e89914db0dfa5cc137f8121929a8376673ec647
jjtfsalgado <jjtfsalgado@gmail.com>
2019-01-08 20:21:42 +0000

    Wip default arguments kata

--- 002_default_arguments.js
+++ 002_default_arguments.js
@@ -1,21 +1,44 @@
 
-function add(a,b) { return a+b;};
+
+var timesFive = function () {
+    var five = 5;
+    return function (a) {
+        return five * x;
+    };
+}();
+
+var x = 1;
 
 function defaultArguments(func, params) {
+    // console.log(func);
+    if(func.prototype) {
+        this.parameters = {};
+        this.func = func;
+        const capture = func.toString().match(/\((...*)\)/);
 
-    this.func = func;
-    this.parameters = Object.values(params);
+        if (capture) {
+            const parameters = capture[1].split(',');
+            parameters.forEach(i => this.parameters[i] = params[i]);
+        }
+    }else{
+        Object.keys(this.parameters).forEach(i => this.parameters[i] = params[i])
+    }
 
     return (...args) => {
-        return this.func.apply(null,args)
+        const parameters = [];
+        Object.keys(this.parameters).forEach((i, index) => parameters.push(args[index] || this.parameters[i]));
+
+        // console.log(parameters)
+        console.log(this.func.apply(null,parameters))
     }
 }
 
-let add_ = defaultArguments(add,{b:9});
-console.log(add_(10,2)); // returns 19
-console.log(add_(10,7)); // returns 17
-console.log(add_()); // returns NaN
+defaultArguments(timesFive,{n:5})();
 
+// add(10); // returns 19
+// console.log(add_(10,7)); // returns 17
+// console.log(add_()); // returns NaN
+//
 // add_ = defaultArguments(add_,{b:3, a:2});
-// add_(10); // returns 13 now
-// add_(); // returns 5
+// console.log(add_(10)); // returns 13 now
+// console.log(add_()); // returns 5
