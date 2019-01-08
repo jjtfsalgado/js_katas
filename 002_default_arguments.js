8ad2dd50987e2284921e0c42cb80ae73b378de51
jjtfsalgado <jjtfsalgado@gmail.com>
2019-01-08 13:05:35 +0000

    Wip default arguments kata

--- 002_default_arguments.js
+++ 002_default_arguments.js
@@ -3,9 +3,19 @@ function add(a,b) { return a+b;};
 
 function defaultArguments(func, params) {
 
+    this.func = func;
+    this.parameters = Object.values(params);
+
+    return (...args) => {
+        return this.func.apply(null,args)
+    }
 }
 
-const add_ = defaultArguments(add,{b:9});
-add_(10); // returns 19
-add_(10,7); // returns 17
-add_(); // returns NaN
+let add_ = defaultArguments(add,{b:9});
+console.log(add_(10,2)); // returns 19
+console.log(add_(10,7)); // returns 17
+console.log(add_()); // returns NaN
+
+// add_ = defaultArguments(add_,{b:3, a:2});
+// add_(10); // returns 13 now
+// add_(); // returns 5
