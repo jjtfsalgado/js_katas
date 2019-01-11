e1af6b6f19b1cc4253fe28c225866a9ea2088fd5
jjtfsalgado <jjtfsalgado@gmail.com>
2019-01-11 14:06:58 +0000

    Wip car park escape kata

--- 003_car_park_escape.js
+++ 003_car_park_escape.js
@@ -1,9 +1,13 @@
-const carpark = [[0, 0, 0, 0, 2]];
+let carpark = [[1, 0, 0, 0, 2],
+    [0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0]];
 
 function escape(carpark){
-    const result = [];
+    let result = [];
     let lastPos;
 
+    //calculate coordinate movements
     for(let i = 0; i < carpark.length; i ++){
         const level = carpark[i];
         const levelNext = carpark[i+1];
@@ -13,6 +17,10 @@ function escape(carpark){
         const stairs = levelNext ? level.findIndex(i => i === 1) : level.length-1;
 
         let start = init !== -1 ? init : lastPos !== stairs ? lastPos : false;
+
+        if(init === stairs){
+            return [];
+        }
 
         if(start || start === 0){
             c1 = start > stairs ? "L" : "R";
@@ -24,9 +32,22 @@ function escape(carpark){
         lastPos = stairs;
     }
 
-    console.log(result)
+    //reduce stairs values
+    return result.reduce((accum, value, index, arr) => {
+        let coord = accum[accum.length-1];
+        let coordNext = value;
+
+        if(coord && coordNext && (coord[0] === "D" && coordNext[0] === "D")){
+            coordNext = coord[0] + (+coord[1] + 1);
+            accum[accum.length - 1] = coordNext
+        }else{
+            accum.push(coordNext || value);
+        }
+
+        return accum;
+    }, []);
 }
 
-escape(carpark);
+console.log(escape(carpark));
 
 // const result = ["L4", "D1", "R4"];
