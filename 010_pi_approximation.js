a93bf9d84ccf96a84e85ec310520817f98c40f66
jjtfsalgado <jjtfsalgado@gmail.com>
2019-02-04 21:54:01 +0000

    Wip pi approximation

--- 010_pi_approximation.js
+++ 010_pi_approximation.js
@@ -1,14 +1,15 @@
 function iterPi(epsilon) {
     const piJs = Math.PI;
     var v = 0;
-    let iter = 0;
-    for (i = 1; i <= 10000000000; i += 4) {  // increment by 4
-        v +=  1 / i - 1 / (i + 2); // add the value of the series
-        const piCalc = v * 4;
-        ++iter
+    var it = 0;
+    for (var j,i = 1; i <= 10000000; ++j,i += 4) {
+        v +=  1 / i - 1 / (i + 2);
+        var pi = v * 4
 
-        if(Math.abs(piJs - piCalc) < epsilon){
-            return [iter * 2, +(piCalc).toFixed(10)]
+        if(Math.abs(piJs - pi) < epsilon){
+            return [j*2, +(pi).toFixed(10)]
         }
     }
 };
+
+console.log(iterPi(0.001))
