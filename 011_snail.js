b5609c66c3dc78dbf0e10779974b50d5e0d7d63d
jjtfsalgado <jjtfsalgado@gmail.com>
2019-02-06 13:48:01 +0000

    Wip snail kata

--- 011_snail.js
+++ 011_snail.js
@@ -2,20 +2,27 @@ snail = (array) => {
     const snail = [];
     const size = array.length;
 
-    for (let i = 0; i < size; i++) {
+    for (let i = 0; i < array.length; i++) {
         const arr = array[i];
-        if(i === 0){
+
+        if((size - i) === array.length){
             snail.push(...arr);
             continue;
         }
 
-        if(i%2 === 0){
+        for (let j = 0; j < arr.length; j++) {
 
+
+            console.log(array[i][j]);
         }
     }
 };
 
-array = [[1,2,3],[4,5,6],[7,8,9]];
-// snail(array) #=> [1,2,3,6,9,8,7,4,5]
+array3 = [ [1,2,3,4,5],
+    [5,6,7,8,3],
+    [9,7,2,4,0],
+    [2,4,5,6,1],
+    [4,7,3,2,9]
+];
 
-console.log(snail(array));
+console.log(snail(array3)); //[1,2,3,4,5,3,0,1,9,2,3,7,4,2,9,5,6,7,8,4,6,5,4,7,2]
