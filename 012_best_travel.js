13c48c213c4f259460eab85ed4809b4121c1fb5b
jjtfsalgado <jjtfsalgado@gmail.com>
2019-02-18 12:58:55 +0000

    Added best travel kata

---
+++ 012_best_travel.js
@@ -0,0 +1,9 @@
+function chooseBestSum(t, k, ls) {
+    // your code
+}
+
+// ts = [50, 55, 56, 57, 58] choose_best_sum(163, 3, ts) -> 163
+//
+// xs = [50] choose_best_sum(163, 3, xs) -> nil (or null or ... or -1 (C++, C, Rust, Swift, Go)
+//
+// ys = [91, 74, 73, 85, 73, 81, 87] choose_best_sum(230, 3, ys) -> 228
