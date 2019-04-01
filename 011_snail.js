53cc04db83193630cf4d4322a4d51a0a36d14ce0
salgadoj <jose.salgado@iplicit.com>
2019-04-01 18:11:23 +0100

    Finished snail kata

--- 011_snail.js
+++ 011_snail.js
@@ -1,11 +1,17 @@
-const snail = (array, result, counter) => {
+const snail = (arr) => {
+    return snailRecur(arr, [], 0)
+}
+
+const snailRecur = (array, result, counter) => {
     result = result || [];
     const size = array.length - counter;
-    console.log(counter);
 
     for (let i = 0; i < 4; i++) {
         if(i === 0){
-            result.push(...array[counter].slice(counter > 0 ? 1 : 0, size))
+            result.push(...array[counter].slice(counter, size));
+            if((counter + 1) === size){
+                return result;
+            }
         }else if(i === 1){
             for (let j = 1 + counter; j < size - 1; j++) {
                 const ar = array[j];
@@ -23,19 +29,25 @@ const snail = (array, result, counter) => {
 
     ++counter;
 
-    if((array.length - counter) > (array.length / 2)){
-        snail(array, result, counter)
+    if(counter < size){
+        snailRecur(array, result, counter)
     }
 
     return result;
 };
+//
+// array3 = [
+//     [1,2,3,4,5],
+//     [5,6,7,8,3],
+//     [9,7,2,4,0],
+//     [2,4,5,6,1],
+//     [4,7,3,2,9]
+// ];
 
 array3 = [
-    [1,2,3,4,5],
-    [5,6,7,8,3],
-    [9,7,2,4,0],
-    [2,4,5,6,1],
-    [4,7,3,2,9]
+    [1,2,3],
+    [2,3,4],
+    [2,3,4]
 ];
 
 console.log(snail(array3, [], 0)); //[1,2,3,4,5,3,0,1,9,2,3,7,4,2,9,5,6,7,8,4,6,5,4,7,2]
