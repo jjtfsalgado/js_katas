152f304f72c207ca37c10ac11c81959f943e18c1
salgadoj <jose.salgado@iplicit.com>
2019-04-02 13:16:12 +0100

    Added kata

---
+++ 014_coding_squared_strings.js
@@ -0,0 +1,15 @@
+const str = "I.was.going.fishing.that.morning.at.ten.o'clock";
+
+function code(s) {
+    return;
+}
+
+
+function decode(s) {
+
+}
+
+const codedS = code(str);
+decode(codedS);
+
+//code -> "c.nhsoI\nltiahi.\noentinw\ncng.nga\nk..mg.s\n\voao.f.\n\v'trtig"
