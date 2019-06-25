40095d489b32151fdbb3812c09141da9176979ad
salgadoj <jose.salgado@iplicit.com>
2019-06-25 16:40:39 +0100

    Wip

--- 016_connect_four.js
+++ 016_connect_four.js
@@ -1,4 +1,4 @@
-//7*5
+
 function whoIsWinner(piecesPositionList) {
     const data = {
         A: [],
@@ -11,8 +11,15 @@ function whoIsWinner(piecesPositionList) {
     };
 
     const plays = piecesPositionList.map(i => i.split("_"));
-    plays.forEach(i => data[i[0]].push(i[1]));
-    return analyseWinner(data)
+
+    for (let i = 0; i < plays.length; i++) {
+        const play = plays[i];
+        data[play[0]].push(play[1]);
+        const res = analyseWinner(data);
+        if(res){
+            return res;
+        }
+    }
 }
 
 function analyseWinner(data){
@@ -20,44 +27,70 @@ function analyseWinner(data){
 
     //check vertical
     for (let i = 0; i < arrays.length; i++) {
-         consecfour(arrays[i])
+         const res = consecfour(arrays[i]);
+         if(res){
+             return res;
+         }
     }
 
     //check horizontal
-    for (let i = 0; i <= 6; i++) {
-        consecfour(arrays.map(j => j[i]))
+    for (let i = 0; i < arrays.length; i++) {
+        const res = consecfour(arrays.map(j => j[i]));
+        if(res){
+            return res;
+        }
     }
 
     //check diagonal
-    traverse(arrays);
-}
-
-function traverse(arrays){
-    const xx = 7;
-    const yy = 6;
-
-    for (let i = xx; i > 0; i--) {
-        const arr = [];
-        for (let j = i ; j < yy; j++) {
-            arr.push(arrays[i][j]);
+    const traverseArrs = traverse(arrays);
+    for (let i = 0; i < traverseArrs.length; i++) {
+        const res = consecfour(traverseArrs[i]);
+        if(res){
+            return res;
         }
-
-        console.log(arr)
-        // consecfour(arrays.map(j => j[i]))
     }
 }
 
+function traverse(arrays){
+    const container = [];
+
+    const xx = arrays.length - 1;
+    const yy = arrays[0].length - 1;
+
+    for (let i = xx; i >= 0; i--) {
+        const arr = [];
+        let diff = xx-i;
+        for (let j = 0 ; j <= (diff > yy ? yy : diff); j++) {
+            arr.push(arrays[i+j][j]);
+        }
+    }
+
+    for (let i = 0; i <= xx; i++) {
+        const arr = [];
+        const diff = yy-i;
+        for (let j = yy ; j >= diff; j--) {
+            arr.push(arrays[i-(yy-j)][j]);
+        }
+        container.push(arr);
+    }
+
+    return container;
+}
+
 function consecfour(arr, dir){
+
+
     for(let i = 0; i < arr.length; i+=4){
         const subArr = arr.slice(i, i+4);
         const item = subArr[0];
         if(item && subArr.every(i => i === item) && subArr.length === 4){
+            // console.log(item)
             return item
         }
     }
 }
 
-whoIsWinner(["C_Yellow",
+console.log(whoIsWinner(["C_Yellow",
     "E_Red",
     "G_Yellow",
     "B_Red",
@@ -84,4 +117,4 @@ whoIsWinner(["C_Yellow",
     "G_Yellow",
     "D_Red",
     "D_Yellow",
-    "C_Red"]);
+    "C_Red"]));
