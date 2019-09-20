b5b899d9ffbf3b0ad60b019916717c80eec21dd6
salgadoj <jose.salgado@iplicit.com>
2019-09-20 14:20:16 +0100

    Wip

--- 018_simple_encryption.js
+++ 018_simple_encryption.js
@@ -1,28 +1,74 @@
 function encrypt(text, n) {
-    return crypt(text, n, true)
-}
-
-function decrypt(encryptedText, n) {
-    return crypt(encryptedText, n)
-}
-
-
-function crypt(text, n, isEncrypt) {
     if(n < 1 || !n){return text};
 
     const arr = text.split("");
     const odds = arr.filter((i, index) => index%2 === 0);
     const evens = arr.filter((i, index) => index%2 !== 0);
 
-    const res = isEncrypt ? evens.concat(odds) : odds.concat(evens);
+    const res = evens.concat(odds);
     const encrypted = res.join("");
 
-    return n > 1 ? crypt(encrypted, --n, isEncrypt) : encrypted;
+    return n > 1 ? encrypt(encrypted, --n) : encrypted;
 }
+
+function decrypt(encryptedText, n) {
+    return encrypt(encryptedText, 4 - n)
+}
+
+//
+// function crypt(text, n, isEncrypt) {
+//     if(n < 1 || !n){return text};
+//
+//     const arr = text.split("");
+//     const odds = arr.filter((i, index) => index%2 === 0);
+//     const evens = arr.filter((i, index) => index%2 !== 0);
+//
+//     const res = isEncrypt ? evens.concat(odds) : odds.concat(evens);
+//     const encrypted = res.join("");
+//
+//     return n > 1 ? crypt(encrypted, --n, isEncrypt) : encrypted;
+// }
 
 const text = "This is a test!";
 
-const encryptedText = encrypt(text, 1);
-console.log(encryptedText);
+console.log(1%4);
 
-console.log(decrypt(encryptedText, 1));
+// console.log(encrypt("This kata is very interesting!", 1));
+// console.log(encrypt("This kata is very interesting!", 2));
+// console.log(encrypt("This kata is very interesting!", 3));
+// console.log(encrypt("This kata is very interesting!", 4));
+const input = "This kata!";
+
+console.log(input.length);
+
+console.log(encrypt(input, 1), 1);
+console.log(encrypt(input, 2), 2);
+console.log(encrypt(input, 3), 3);
+console.log(encrypt(input, 4), 4);
+console.log(encrypt(input, 5), 5);
+console.log(encrypt(input, 6), 6);
+console.log(encrypt(input, 7), 7);
+console.log(encrypt(input, 8), 8);
+console.log(encrypt(input, 9), 9);
+console.log(encrypt(input, 10), 10);
+console.log(encrypt(input, 11), 11);
+console.log(encrypt(input, 12), 12);
+console.log(encrypt(input, 13), 13);
+console.log(encrypt(input, 14), 14);
+console.log(encrypt(input, 15), 15);
+console.log(encrypt(input, 16), 16);
+console.log(encrypt(input, 17), 17);
+console.log(encrypt(input, 18), 18);
+console.log(encrypt(input, 19), 19);
+console.log(encrypt(input, 20), 20);
+console.log(encrypt(input, 21), 21);
+console.log(encrypt(input, 22), 22);
+console.log(encrypt(input, 23), 23);
+console.log(encrypt(input, 24), 24);
+console.log(encrypt(input, 25), 25);
+console.log(encrypt(input, 26), 26);
+
+
+
+
+// console.log(decrypt(encryptedText, 1));
