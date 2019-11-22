df2a0990bad1162f196d30586db565952787d7a1
salgadoj <jose.salgado@iplicit.com>
2019-11-22 14:31:32 +0000

    wip

--- 012_best_travel[INCOMPLETE].js
+++ 012_best_travel[INCOMPLETE].js
@@ -1,67 +1,21 @@
-function chooseBestSum(t, k, ls) {
+const array = [50, 55, 57, 58, 60 ];
 
-    let sum = null;
-
-
-
-    // for (let j = 0; j < ls.length; j++) {
-    //
-    //     if((j + k - 1) > ls.length){
-    //         continue
-    //     }
-    //
-    //     const subArr = ls.slice(j, j + k - 1);
-    //
-    //     for (let i = 0; i < ls.length; i++) {
-    //         const l = ls[i];
-    //
-    //         if(subArr.includes(l)){
-    //             continue;
-    //         }
-    //
-    //         const arr = subArr.concat(l);
-    //         const s = sumArr(arr);
-    //
-    //         if(s > sum && s <= t ){
-    //             sum = s
-    //         };
-    //     }
-    // }
-
-    console.log(sum, t, k , ls);
-
-    return sum;
-}
-
-function sumArr(arr){
-    return arr.reduce((accum, value) => accum += value, 0)
-}
-
-ts = [ 91, 74, 73, 85, 73, 81, 87 ];
+const container = [];
 
 
-const combine = function(a, min) {
-    var fn = function(n, src, got, all) {
-        if (n == 0) {
-            if (got.length > 0) {
-                all[all.length] = got;
-            }
-            return;
-        }
-        for (var j = 0; j < src.length; j++) {
-            fn(n - 1, src.slice(j + 1), got.concat([src[j]]), all);
-        }
-        return;
+
+function recur(arr,n, start, ...rest){
+    const c = [];
+
+
+    for (let j = start; j < n; j++) {
+        c.push(arr[j])
     }
-    var all = [];
-    for (var i = min; i < a.length; i++) {
-        fn(i, a, [], all);
-    }
-    all.push(a);
-    return all;
+
+
+    console.log(c)
+
 }
 
 
-console.log(combine(ts, 1));
-
-console.log(chooseBestSum(331, 4, ts));
+console.log(recur(array,3, 0));
