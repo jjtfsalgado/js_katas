ec1328f01dcb8fcf37a07563b8dbed3862f8ce2c
jjtfsalgado <jjtfsalgado@gmail.com>
2019-11-23 09:57:40 +0000

    Wip

--- 012_best_travel[INCOMPLETE].js
+++ 012_best_travel[INCOMPLETE].js
@@ -2,14 +2,16 @@ const array = [50, 55, 57, 58, 60 ];
 const n = 3;
 const container = [];
 
-function loop(arr, start) {
+let count = 0;
 
+function loop(arr, start) {
+    ++count;
 
     for (let i = start || 0; i < array.length; i++) {
         const a = arr.concat(i);
 
         if (a.length < n) {
-            loop(a, start + 1);
+            loop(a, a[a.length - 1] + 1);
         }else if(a.length === n){
             container.push(a);
         }
@@ -18,9 +20,10 @@ function loop(arr, start) {
 
 function start(){
     for (let i = 0; i < n; i++) {
-        loop([i], i + 1);
+        loop([array[i]], i + 1);
     }
     console.log(container)
+    console.log(count)
 }
 
 
